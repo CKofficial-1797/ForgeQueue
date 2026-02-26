dc2681a0687312c32568891b08ff76c1b4417eee
CKofficial-1797 <sanjaygorain1797@gmail.com>
2026-02-26 11:05:40 +0530

    bootstrap: initialize ForgeQueue core service with Spring Boot baseline

---
+++ forgequeue-core/src/main/java/com/forgequeue/core/ForgequeueCoreApplication.java
@@ -0,0 +1,13 @@
+package com.forgequeue.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgequeueCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgequeueCoreApplication.class, args);
+	}
+
+}
